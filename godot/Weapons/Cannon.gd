469541f4d3156c95a436740ebd77fc63ed989e10
Dane Johnson <dane_johnson@ymail.com>
2021-08-20 14:46:48 -0500

    Bird must reload

--- godot/Weapons/Cannon.gd
+++ godot/Weapons/Cannon.gd
@@ -1,7 +1,31 @@
 extends Weapon
 
+func _ready():
+	._ready() ## Call super
+	$ReloadTimer.connect("timeout", self, "on_reload_complete")
+
 func fire():
-	$AnimationPlayer.play("Firing")
+	if reloading: return
+	if ammo_in_clip > 0:
+		$AnimationPlayer.play("Firing")
+	else:
+		reload()
+
+func on_shot():
+	if ammo_in_clip > 0:
+		ammo_in_clip -= 1
+	else:
+		unfire()
+
+func reload():
+	if $ReloadTimer.is_stopped() and ammo_in_clip < shots_per_clip:
+		reloading = true
+		$ReloadTimer.start()
+		$ReloadSound.play()
 
 func unfire():
 	$AnimationPlayer.stop()
+
+func on_reload_complete():
+	reloading = false
+	ammo_in_clip = shots_per_clip
