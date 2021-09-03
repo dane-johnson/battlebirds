1ec148cb85d9790e78dbc6d9fab5f9e807f4cfd0
Dane Johnson <dane_johnson@ymail.com>
2021-09-03 09:31:05 -0500

    Bird only falls down

--- godot/Characters/BirdMovementController.gd
+++ godot/Characters/BirdMovementController.gd
@@ -1,14 +1,10 @@
 extends MovementController
 
-var jet_speed = 100.0
+const jet_speed = 100.0
 
 func run_physics(delta):
 	if body.flight_mode == "empty":
-		var flat_vel = Vector3(velocity.x, 0, velocity.y)
-		if flat_vel.length_squared() < 0.25:
-			velocity.x = 0
-			velocity.z = 0
-		velocity = body.move_and_slide(velocity + gravity * delta, Vector3.UP)
+		body.move_and_collide(gravity * jet_speed * delta * delta)
 	if body.flight_mode == "hover":
 		body.move_and_slide(move_vec * move_accel, Vector3.UP)
 	if body.flight_mode == "jet":
